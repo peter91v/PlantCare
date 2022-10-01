7b75b0ce0040ee5a46f9d802eb96ee79e847404b
peter91v <varga.pter91@gmail.com>
2022-10-01 11:40:22 +0200

    edited connectDatabase

--- plant.care/src/plant/care/browser/utils/py/connectDatabase.py
+++ plant.care/src/plant/care/browser/utils/py/connectDatabase.py
@@ -3,16 +3,15 @@ import mysql.connector
 
 
 def connectToDatabase():
-    mysql1 = mysql.connector.connect(user='peter', password='plantcare',
-                              host='localhost',
+    mysql1 = mysql.connector.connect(user='plantcare', password='plantcare',
+                              host='192.168.178.85',
                               database='plantcare')
-    # if(mysql1):
-    #     print("connection successfull")
-    # else:
-    #     print("failed")
+    if(mysql1):
+        print("connection successfull")
+    else:
+        print("failed")
 
     return mysql1
 
 def closeDatabase(mysql1):
     mysql1.close()
-
